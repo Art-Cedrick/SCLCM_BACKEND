c93a00039d51274da0c29895d44629c81392c33a
Chester Jazmin <chestervjazmin@gmail.com>
2025-02-09 20:04:26 +0800

    add update

--- api/views.py
+++ api/views.py
@@ -920,14 +920,12 @@ class GetRecordView(APIView):
         except IndividualRecordForm.DoesNotExist:
             return Response([], status=status.HTTP_200_OK)
         
-    def post(self, request):
-        data = request.data.copy()
-        data['profile'] = request.user.profile.id
-        data['sr_code'] = request.data.get('sr_code')
-        serializer = IndividualRecordFormSerializer(data=data)
+    def patch(self, request):
+        record = IndividualRecordForm.objects.get(profile=request.user.profile.id)
+        serializer = IndividualRecordFormSerializer(record, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
